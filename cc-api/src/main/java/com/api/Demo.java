a1ab849ec5cfe13349d824df776e54dd5ccbdd4a
apria <chenyahui@dookbook.com>
2019-11-19 18:12:43 +0800

    api

---
+++ cc-api/src/main/java/com/api/Demo.java
@@ -0,0 +1,13 @@
+package com.api;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: yahui
+ * Date: 2019/10/28
+ * To change this template use File | Settings | File Templates.
+ **/
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("api");
+    }
+}
